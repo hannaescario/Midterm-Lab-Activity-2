38932edef0023ab9be8f73f4dd29138752bb6233
hannaescario <100050898+hannaescario@users.noreply.github.com>
2022-03-04 11:38:57 +0800

    Update CandyMachine.java

--- CandyMachine.java
+++ CandyMachine.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class CandyMachine{
 
-static Scanner console new Scanner(System.in); 
+static Scanner console = new Scanner(System.in); 
 
  public static void main(String1] args){
 
